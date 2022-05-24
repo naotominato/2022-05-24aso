c2f2cf091d9dc7e1c64c7ab58818039532da5674
naoto <a_naoto@outlook.jp>
2022-05-24 20:37:07 +0900

    second

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ target.addEventListener('click', () => {
   target.classList.toggle('open');
   const headerNav = document.getElementById("header__nav");
   headerNav.classList.toggle('in');
-})
+});
